141ec771637c877c7d12fd15b8f557bcee69cf87
Anastasia Kogteva <kogteva.nastya@gmail.com>
2020-04-28 21:09:23 +0300

    investigated selenium webdriver, added simple ui tests

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,48 @@
-const { Builder } = require('selenium-webdriver');
-require('selenium-webdriver/chrome');
-require('chromedriver');
-require('geckodriver');
+describe('test application page', () => {
+  const { Builder, By, until } = require('selenium-webdriver');
+  var assert = require('assert');
+  require('chromedriver');
+  var driver;
 
-const rootURL = 'http://localhost:8080/'
-const d = new Builder().forBrowser('chrome').build()
-let driver;
+  beforeAll(() => {
+    driver = new Builder()
+      .forBrowser('chrome')
+      .build();
+  })
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
+  beforeEach(async () => {
+    await driver.get('http://localhost:8080/');
+  })
 
-it('waits for the driver to start', async () => driver = await d)
-it('opens the webpage', async () => await driver.get(rootURL))
+  afterAll(() => {
+    driver.quit();
+  })
+
+  it('should open approve dialog', async () => {
+    driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/button")).click();
+    var dialogTitle = driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[1]/div[3]/div/div/div[1]/h5")).getText();
+    await dialogTitle.then((text) => {
+      assert.equal("Подтверждение заявки", text);
+    });
+  });
+
+  it('should open delete dialog', async () => {
+    driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[2]/button")).click();
+    var dialogTitle = driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[1]/div[3]/div/div/div[1]/h5")).getText();
+    await dialogTitle.then((text) => {
+      assert.equal("Отклонение заявки", text);
+    });
+  });
+
+  it('should open edit page', async () => {
+    driver.wait(until.elementLocated(By.xpath("//*[@id='root']/div/div[2]/div/div[1]/div[2]/div/div[2]/div/a/div/button"), 5000));
+    driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[1]/div[2]/div/div[2]/div/a/div/button")).click();
+    var pageTitle = driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[1]")).getText();
+    await pageTitle.then((text) => {
+      assert.equal("Редактирование заявки", text);
+    });
+  });
+
+})
+
+
